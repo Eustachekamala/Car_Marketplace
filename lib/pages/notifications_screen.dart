212e49a0deffb8e57ab1bfd01127f3e0b035b0c5
eustachekamala <eustachejared25@gmail.com>
2026-04-20 11:39:59 +0300

    3. Updated the notifications page in the appbar widget

--- lib/pages/notifications_screen.dart
+++ lib/pages/notifications_screen.dart
@@ -1,3 +1,4 @@
+import 'package:car_marketplace/widgets/circle_icon_button_widget.dart';
 import 'package:flutter/material.dart';
 
 class NotificationsScreen extends StatelessWidget {
@@ -6,10 +7,20 @@ class NotificationsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Notifications")),
-      body: const Center(
-        child: Text("No notifications yet"),
+      appBar: AppBar(
+        title: const Text("Notifications"),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 12),
+          child: CircleIconButtonWidget(
+            icon: Icons.arrow_back_ios_new,
+            onPressed: () => Navigator.pop(context),
+          ),
+        ),
       ),
+      body: const Center(child: Text("No notifications yet")),
     );
   }
 }
